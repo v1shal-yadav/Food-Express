65134f1719055f4c6101226792af3b21d4c4c04a
Vishal Yadav <yaimvishal@gmail.com>
2024-06-25 12:01:24 +0530

    payment confirmation setup

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -2,6 +2,11 @@ import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js"
 import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+const currency = "inr"
+const deliveryCharge = 50; 
+const frontend_URL = "https://food-express-frondend.onrender.com"
+
 // Placing User Order for Frontend
 const placeOrder = async (req, res) => {
 
